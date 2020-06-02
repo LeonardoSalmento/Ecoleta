e3c7c74718062b01fd7d7f3e276abf0890c0a2da
LeonardoSalmento <salmento_@hotmail.com>
2020-06-02 15:59:46 -0300

    Adicionando cadastro de pontos e transactions

--- src/routes.ts
+++ src/routes.ts
@@ -19,5 +19,43 @@ routes.get('/items', async (request, response)=>{
     return response.json(serializedItems);
 });
 
+routes.post('/points', async(request, response) =>{
+    const {
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+        items
+    } = request.body;
+
+    const trx = await knex.transaction();
+
+    const insertedIds = await trx('points').insert({
+        image: 'image-fake',
+        name,
+        email,
+        whatsapp,
+        latitude,
+        longitude,
+        city,
+        uf,
+    })
+
+    const point_id = insertedIds[0]; 
+
+    const pointItems = items.map((item_id: Number) =>{
+        return{
+            item_id,
+            point_id,
+        }
+    })
+
+    await trx('points_items').insert(pointItems);
+
+    return response.json({success:true})
+});
 
 export default routes;
