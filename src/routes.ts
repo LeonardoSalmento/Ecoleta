7580aafe10bb870bc01a7474a1b34f4e375b72ac
LeonardoSalmento <salmento_@hotmail.com>
2020-06-05 15:01:16 -0300

    Configurando aplicação para suportar upload de imagem

--- src/routes.ts
+++ src/routes.ts
@@ -1,17 +1,22 @@
 import express from 'express';
 import knex from './database/connection'
+import multer from 'multer';
 
 import PointsController from './controllers/pointsController';
 import ItemsController from './controllers/itemsController';
+
+import multerConfig from './config/multer';
+
+const upload = multer(multerConfig);
 
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 const routes = express.Router();
 
 routes.get('/items', itemsController.index);
-
-routes.post('/points', pointsController.create);
 routes.get('/points', pointsController.index);
 routes.get('/points/:id', pointsController.show);
+
+routes.post('/points', upload.single('image'), pointsController.create);
 
 export default routes;
